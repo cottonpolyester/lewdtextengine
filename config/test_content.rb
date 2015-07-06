27f60b33d56e57478b465ef86151ed46ddc7a999
cottonpolyester <cottonpolyester@cock.li>
2015-07-06 13:48:33 -0400

    Move the new game treestore declaration from bin/lte to resources/lte.glade
    The treestore was previously initialized in the ruby file bin/lte, moved it to
    the xml (glade) resource file for consistency.

--- config/test_content.rb
+++ config/test_content.rb
@@ -1,9 +1,19 @@
 require 'lte'
 
-LTE.add_content LTE::Content.new(
-        "test",
-        "A test content object",
-        lambda do |game|
-                LTE::Game.new(LTE::Character.new("Player Character"), LTE::Location.new("Somewhere"))
-        end
-)
+module LTE
+        @@content << LTE::Content.new(
+                "Test Content #1",
+                "The first test content object",
+                lambda do |game|
+                        LTE::Game.new(LTE::Character.new("Player Character"), LTE::Location.new("Somewhere"))
+                end
+        )
+
+        @@content << LTE::Content.new(
+                "Test Content #2",
+                "The second test content object",
+                lambda do |game|
+                        LTE::Game.new(LTE::Character.new("Player Character"), LTE::Location.new("Somewhere"))
+                end
+        )
+end
--- lib/lte/content.rb
+++ lib/lte/content.rb
@@ -1,37 +1,24 @@
 require 'gtk3'
 
 module LTE
-	@@content = {}
-
 	class Content
 		attr_accessor :name, :description, :active
 
-		def initialize(name, description, game_mod)
-			@name        = name
-			@description = description
-                        @active      = true
-
-			@game_mod_lambda = game_mod
+		def initialize(name, desc, l)
+                        @name = name
+                        @description = desc
+                        @lambda = l
+                        @active = false
 		end
 
 		def mod_game(game)
-			@game_mod_lambda.call(game)
+			@lambda.call(game)
 		end
 	end
+
+	@@content = []
 
         def self.content
-                return @@content
+                @@content
         end
-
-	def self.add_content(content)
-		@@content[content.name] = content
-	end
-
-	def self.del_content(content)
-		if @@content.respond_to? :name
-			@@content.delete content.name
-		else
-			@@content.delete content
-		end
-	end
 end
